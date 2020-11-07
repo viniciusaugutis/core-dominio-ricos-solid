36e9b88ee8804af2b44b2ed371e2b91d7bf4b64e
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-11-07 16:04:21 -0300

    Implementando teste automatizado para queries

---
+++ PaymentContext.Tests/Queries/StudentQueriesTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTest
+    {
+        private IList<Student> _students = new List<Student>();
+
+        public StudentQueriesTest()
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                var student = new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("11111111111", PaymentContext.Domain.Enums.EDocumentoType.CPF),
+                    new Email(i.ToString() + "@teste.com"));
+
+                _students.Add(student);
+            }
+        }
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("1234567891011");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null, studn);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("11111111111");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreNotEqual(null, studn);
+        }
+    }
+}
