58dad5c98f9fe2879bd9a2cf81c760884ee93fce
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-11-07 15:33:08 -0300

    Criando expressao com queries

---
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,17 @@
+﻿using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PaymentContext.Domain.Queries
+{
+    public static class StudentQueries
+    {
+        public static Expression<Func<Student, bool>> GetStudentInfo(string document)
+        {
+            return x => x.Document.Number == document;
+        }
+
+    }
+}
